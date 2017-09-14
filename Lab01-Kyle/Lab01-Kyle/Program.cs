1c316cea8eba22440d1a7d310959499377d2f510
Kyle Richmond <kylerichmond79@gmail.com>
2017-09-13 19:21:50 -0700

    Day02 Lab assignment completed with the refactors from Day01 to be running correctly as well as finishing the requirements for Day02.  My Day02 README.md is added to the original .md.

--- Lab01-Kyle/Lab01-Kyle/Program.cs
+++ Lab01-Kyle/Lab01-Kyle/Program.cs
@@ -21,23 +21,25 @@ namespace Lab01_Kyle
             int AnswerCounter = 0;
             string[] CorrectAnswer = new string[] { "indiana" };
 
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 3; i++)
             {
                 string response = Console.ReadLine().ToLower();
                 if (CorrectAnswer.Contains(response))
                 {
                     Console.WriteLine("You are Correct! I was born in Indiana.");
                     AnswerCounter++;
-                    i = 2;
+                    i = 3;
                 }
                 else if (!CorrectAnswer.Contains(response))
                 {
+                    if (i == 2)
+                    {
+                        Console.WriteLine("I am sorry, you are out of guesses.");
+                    }
+                    else
+                    {
                     Console.WriteLine("Sorry please try again!");
-
-                }
-                else if (i == 2)
-                {
-                    Console.WriteLine("I am sorry, you are out of guesses.");
+                    }
                 }
                 else
                 {
@@ -73,8 +75,68 @@ namespace Lab01_Kyle
             int correctAnswerTotal = 0;
             correctAnswerTotal += QuestionOne(); 
             correctAnswerTotal += QuestionTwo();
+            correctAnswerTotal += QuestionThree();
+
             Console.WriteLine($"You got {correctAnswerTotal} Correct!");
             //needed a change to commit branch
+        }
+
+        static int QuestionThree()
+        {
+
+            Console.WriteLine("Can you guess my favorite number?");
+            int AnswerCounter = 0;
+            int CorrectAnswer = 27;
+            int response = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                if (CorrectAnswer == response)
+                {
+                    Console.WriteLine("Way to go, You got it!");
+                    AnswerCounter++;
+                }
+                else if (CorrectAnswer != response)
+                {
+                    Console.WriteLine("I am sorry the correct answer is 27.");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Please enter a number!");
+                QuestionThree();
+            }
+            return AnswerCounter;
+        }
+
+        static int QuestionFour()
+        {
+            Console.WriteLine("Did you enjoy my quiz?");
+            string[] CorrectAnswer = new string[] { "No", "N" };
+            string response = Console.ReadLine().ToLower();
+            int AnswerCounter = 0;
+
+            try
+            {
+                if (CorrectAnswer.Contains(response))
+                {
+                    Console.WriteLine("I am glad you enjoyed");
+                    AnswerCounter++;
+                }
+                else
+                {
+                    Console.WriteLine("I am sorry you didn't enjoy my quiz.");
+                }
+            }
+            finally {
+                Console.WriteLine("Fine Be that way!");
+            }
+        return AnswerCounter;
         }
     }
 }
